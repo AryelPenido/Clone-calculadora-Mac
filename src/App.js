998043b10b6495566b0d6175f3529c3745ed5119
Aryel Penido <aryelpenido@gmail.com>
2020-06-17 10:13:57 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './main/Calculator'
+import Calculator from './main/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+     <Calculator/>
+    </div>
+  );
+}
+
+export default App;
